77a89390ac24a8c10b97f6cef8b55e6e9a53f52b
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-07-22 22:57:01 -0300

    feat: requisito 7 OK

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT 
+  ar.artista AS "artista",
+  al.album AS "album",
+  COUNT(*) AS "seguidores"
+FROM SpotifyClone.artistas AS ar
+INNER JOIN  SpotifyClone.albuns AS al ON ar.artista_id = al.artista_id
+INNER JOIN SpotifyClone.seguindo_artista AS seg ON seg.artista_id = ar.artista_id
+GROUP BY al.album, seg.artista_id
+ORDER BY COUNT(*) DESC, ar.artista, al.album;
