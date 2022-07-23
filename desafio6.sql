122e6c4008a126f2758922772d74f19c2174f0ac
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-07-22 22:31:28 -0300

    feat: requisito 6 OK

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT 
+  ROUND(MIN(p.plano_valor), 2) AS "faturamento_minimo",
+  ROUND(MAX(p.plano_valor), 2) AS "faturamento_maximo",
+  ROUND(AVG(plano_valor), 2) AS "faturamento_medio",
+  ROUND(SUM(plano_valor), 2) AS "faturamento_total"
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.planos AS p ON u.plano_id = p.plano_id;
