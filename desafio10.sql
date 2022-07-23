c4ce394589101dc7cf1631971fd1feec1a07f1d8
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-07-22 23:15:13 -0300

    feat: requisito 10 OK e correção da formatação do 9

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT 
+  m.titulo AS "nome",
+  COUNT(*) AS "reproducoes"
+FROM SpotifyClone.reproducao AS rep
+INNER JOIN SpotifyClone.usuario AS ur ON rep.usuario_id = ur.usuario_id
+INNER JOIN SpotifyClone.musicas AS m ON m.musica_id = rep.musica_id
+WHERE plano_id IN (1, 4)
+GROUP BY m.titulo
+ORDER BY COUNT(*) DESC, m.titulo;
